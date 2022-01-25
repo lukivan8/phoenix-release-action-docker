5b5871f948432cd650fdd039f0d31c7927d8a50d
Aaron Sutton <hey@aaron.as>
2022-01-25 16:27:36 -0500

    IIFE

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Build a Phoenix project using Elixir releases.
 
 See the [Phoenix documentation](https://hexdocs.pm/phoenix/releases.html) and the [Mix documentation](https://hexdocs.pm/mix/Mix.Tasks.Release.html) for more information on this process.
+
+## Compatibility
+
+Requires at least Phoenix 1.6.3 or higher and Elixir 1.12 or higher.
+
+## Usage
--- src/phoenix-release.ts
+++ src/phoenix-release.ts
@@ -2,23 +2,27 @@ import * as os from 'os'
 import { locateMixFile, parseMixFile } from './mix'
 
 import * as core from '@actions/core'
-import { exec } from '@actions/exec'
+import * as exec from '@actions/exec'
 
 let mixFile: string | undefined
 
-try {
-	mixFile = locateMixFile('../github_actions_test')
-} catch (err: any) {
-	core.setFailed(`Action failed with error ${err.message}`);
+const run = async () => {
+	try {
+		mixFile = locateMixFile('../github_actions_test')
+	} catch (err: any) {
+		core.setFailed(`Action failed with error ${err.message}`);
+	}
+
+	const mixProject = await parseMixFile(mixFile!)
+
+	const target = [os.arch(), os.platform(), os.release()].join('-')
+	const app =  [mixProject.app, mixProject.version].join('-')
+
+	core.info("Mix application: " + app)
+	core.info("Build target: " + target)
+
+	await exec.exec("mix phx.gen.release")
+	await exec.exec("mix release")
 }
 
-const mixProject = await parseMixFile(mixFile!)
-
-const target = [os.arch(), os.platform(), os.release()].join('-')
-const app =  [mixProject.app, mixProject.version].join('-')
-
-core.info("Mix application: " + app)
-core.info("Build target: " + target)
-
-await exec("mix phx.gen.release")
-await exec("mix release")
+await run();
